68a2a5eca2c4448791fb14ac22e76ea097b687d5
Geison de Souza <geisonszo@gmail.com>
2019-02-15 12:27:46 -0200

    Sessão 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) { 
 
@@ -18,11 +19,60 @@ export class ProductosService {
 
   private cargarProductos() {
 
-    this.http.get('https://portfolio-1b5df.firebaseio.com/productos_idx.json')
+    return new Promise( ( resolve, reject ) => {
+
+      this.http.get('https://portfolio-1b5df.firebaseio.com/productos_idx.json')
     .subscribe( (resp: Producto[]) => {
-      console.log(resp);
       this.productos = resp;
       this.cargando = false;
+      resolve();
     });
+
+    } );
+
+    
+  }
+
+  getProducto(id: string) {
+
+    return this.http.get(`https://portfolio-1b5df.firebaseio.com/productos/${ id }.json`)
+  }
+
+  buscarProducto(termino: string) {
+
+    if (this.productos.length === 0) {
+      // cargar productos
+      this.cargarProductos().then( () => {
+        // Executar depois de obter os productos
+        // aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    } else {
+      // aplicar filtro
+      this.filtrarProductos(termino);
+    }
+    
+  }
+
+  private filtrarProductos(termino: string) {
+
+    console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if (prod.categoria.indexOf(termino) >= 0 || prod.titulo.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+      }
+    });
+    
+
+    
   }
 }
